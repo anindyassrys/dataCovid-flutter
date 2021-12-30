2055de7427c9570db8b292bb6887277d457dbcac
M. Margaretha Stella <stellaa.mmskdt@gmail.com>
2021-12-31 04:48:33 +0700

    masih error

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:covid_info_app/screen/auth.dart';
+
+import 'package:covid_info_app/screen/login_page.dart';
+import 'package:covid_info_app/screen/register_page.dart';
+import 'package:covid_info_app/screen/user_profile.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -10,36 +18,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return Provider(
+        create: (_) {
+          CookieRequest request = CookieRequest();
+
+          return request;
+        },
+        child: MaterialApp(
+            title: 'PBP09',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.brown,
+            ),
+            // home: const MyHomePage(title: 'PBO09'),
+            initialRoute: '/',
+            routes: {
+              '/': (ctx) => const MyHomePage(title: 'PBP09'),
+              LoginScreen.routeName: (ctx) => const LoginScreen(),
+              RegisterScreen.routeName: (ctx) => const RegisterScreen(),
+            }));
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +50,129 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _pageIndex = 0;
 
-  void _incrementCounter() {
+  List<Widget> _pages = [];
+  PageController _pageController = PageController(initialPage: 0);
+
+  @override
+  void initState() {
+    super.initState();
+    // Page index for determining menu active state.
+    _pageIndex = 0;
+
+    _pages = <Widget>[
+      const RegisterScreen(),
+      const LoginScreen(),
+      const Profile()
+      // const Dashboard(),
+      // const AccountScreen(),
+      //       Dashboard(updatePage: (int x) {
+      //   _setPage(x);
+      //   _pageController.jumpToPage(x);
+      // }),
+      // const Profile()
+    ];
+
+    // Page controller
+    _pageController = PageController(initialPage: _pageIndex);
+
+    // checkAuth();
+  }
+
+  // Future<void> checkAuth() async {
+  //   if (req.loggedIn) {
+  //     setState(() {
+  //       _pages[4] = const AccountAuthedScreen();
+  //     });
+  //   }
+  // }
+
+  void _setPage(int x) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _pageIndex = x;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final request = context.watch<CookieRequest>();
+    request.init(context);
+
+    if (request.loggedIn) {
+      _pages[0] = const Profile();
+    }
+
+    List<BottomNavigationBarItem> menuItems = const [
+      BottomNavigationBarItem(
+        icon: Padding(
+            child: Icon(Icons.home_outlined),
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 2.0)),
+        activeIcon: Padding(
+            child: Icon(Icons.home),
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 2.0)),
+        label: 'Home',
+      ),
+      BottomNavigationBarItem(
+        icon: Padding(
+            child: Icon(Icons.dashboard_outlined),
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 2.0)),
+        activeIcon: Padding(
+            child: Icon(Icons.dashboard),
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 2.0)),
+        label: 'Dashboard',
+      ),
+      BottomNavigationBarItem(
+          icon: Padding(
+              child: Icon(Icons.person_outline),
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 2.0)),
+          activeIcon: Padding(
+              child: Icon(Icons.person),
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 2.0)),
+          label: 'Profile')
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Row(children: [
+          // Container(
+          //     child: Image.asset(
+          //       "assets/images/logo.png",
+          //       width: 36.0,
+          //       height: 36.0,
+          //     ),
+          //     margin: const EdgeInsets.fromLTRB(0, 0, 6.0, 0)),
+          Text(widget.title,
+              style: const TextStyle(fontWeight: FontWeight.w600)),
+        ]),
+        backgroundColor: const Color.fromARGB(255, 200, 193, 190),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: PageView(
+        controller: _pageController,
+        children: _pages,
+        onPageChanged: (page) {
+          setState(() {
+            _pageIndex = page;
+          });
+        },
       ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: SizedBox(
+          child: BottomNavigationBar(
+              elevation: 2.5,
+              backgroundColor: const Color.fromARGB(255, 200, 193, 190),
+              items: menuItems,
+              currentIndex: _pageIndex,
+              unselectedItemColor: const Color(0xFF807166),
+              selectedItemColor: const Color(0xFF4B6148),
+              onTap: (int x) {
+                _pageController.jumpToPage(x);
+                _setPage(x);
+              },
+              selectedFontSize: 13.0,
+              iconSize: 25.0,
+              unselectedFontSize: 13.0,
+              type: BottomNavigationBarType.fixed),
+          height: 60),
     );
   }
 }
---
+++ lib/screen/register_page.dart
@@ -0,0 +1,364 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:provider/provider.dart';
+
+import 'package:covid_info_app/screen/auth.dart';
+
+void _showErrors(context, errors) {
+  showDialog(
+      context: context,
+      builder: (context) {
+        return SimpleDialog(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  for (var errorList in errors.values)
+                    for (var error in errorList)
+                      Column(children: [
+                        Text(error.toString()),
+                        const SizedBox(
+                          height: 5,
+                        )
+                      ]),
+                ],
+              ),
+            ),
+          ],
+        );
+      });
+}
+
+class RegisterScreen extends StatefulWidget {
+  static const routeName = '/register';
+
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _registerFormKey = GlobalKey<FormState>();
+
+  bool isPasswordVisible = false;
+
+  void togglePasswordView() {
+    setState(() {
+      isPasswordVisible = !isPasswordVisible;
+    });
+  }
+
+  String first_name = "";
+  String last_name = "";
+  String username = "";
+  String email = "";
+  String password1 = "";
+  String password2 = "";
+
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+
+    return Scaffold(
+        backgroundColor: const Color.fromRGBO(237, 234, 229, 1.0),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Create New Account',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 19, 19, 19),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                const SizedBox(
+                  height: 30,
+                ),
+                // Form
+                Form(
+                  key: _registerFormKey,
+                  child: Column(
+                    children: [
+                      // First Name
+                      Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(250, 250, 250, 0.95),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: TextFormField(
+                            onChanged: (String value) {
+                              first_name = value;
+                            },
+                            decoration: const InputDecoration(
+                              hintText: 'Enter your first name',
+                              hintStyle: TextStyle(
+                                color: Color.fromRGBO(200, 200, 200, 1),
+                              ),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "First name cannot be empty";
+                              }
+                              return null;
+                            },
+                          )),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      // First Name
+                      Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(250, 250, 250, 0.95),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: TextFormField(
+                            onChanged: (String value) {
+                              last_name = value;
+                            },
+                            decoration: const InputDecoration(
+                              hintText: 'Enter your last name',
+                              hintStyle: TextStyle(
+                                color: Color.fromRGBO(200, 200, 200, 1),
+                              ),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Last name cannot be empty";
+                              }
+                              return null;
+                            },
+                          )),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      // Username
+                      Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(250, 250, 250, 0.95),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: TextFormField(
+                            onChanged: (String value) {
+                              username = value;
+                            },
+                            decoration: const InputDecoration(
+                              hintText: 'Username',
+                              hintStyle: TextStyle(
+                                color: Color.fromRGBO(200, 200, 200, 1),
+                              ),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Username cannot be empty";
+                              }
+                              return null;
+                            },
+                          )),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      // Email
+                      Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(250, 250, 250, 0.95),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: TextFormField(
+                            onChanged: (String value) {
+                              email = value;
+                            },
+                            decoration: const InputDecoration(
+                              hintText: 'Email',
+                              hintStyle: TextStyle(
+                                color: Color.fromRGBO(200, 200, 200, 1),
+                              ),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Email cannot be empty";
+                              } else if (!EmailValidator.validate(value)) {
+                                return "Hmm... That email doesn\'t look valid";
+                              }
+                              return null;
+                            },
+                          )),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      // Password
+                      Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(250, 250, 250, 0.95),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: TextFormField(
+                            onChanged: (String value) {
+                              password1 = value;
+                            },
+                            obscureText: !isPasswordVisible,
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                              hintStyle: const TextStyle(
+                                color: Color.fromRGBO(200, 200, 200, 1),
+                              ),
+                              suffixIcon: IconButton(
+                                color: const Color.fromRGBO(200, 200, 200, 1),
+                                splashRadius: 1,
+                                icon: Icon(isPasswordVisible
+                                    ? Icons.visibility_outlined
+                                    : Icons.visibility_off_outlined),
+                                onPressed: togglePasswordView,
+                              ),
+                              border: const OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Password cannot be empty";
+                              }
+                              return null;
+                            },
+                          )),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      // Confirm Password
+                      Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromRGBO(250, 250, 250, 0.95),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: TextFormField(
+                          onChanged: (String value) {
+                            password2 = value;
+                          },
+                          obscureText: !isPasswordVisible,
+                          decoration: InputDecoration(
+                            hintText: 'Confirm Password',
+                            hintStyle: const TextStyle(
+                              color: Color.fromRGBO(200, 200, 200, 1),
+                            ),
+                            suffixIcon: IconButton(
+                              color: const Color.fromRGBO(200, 200, 200, 1),
+                              splashRadius: 1,
+                              icon: Icon(isPasswordVisible
+                                  ? Icons.visibility_outlined
+                                  : Icons.visibility_off_outlined),
+                              onPressed: togglePasswordView,
+                            ),
+                            border: const OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Password cannot be empty";
+                            } else if (value != password1) {
+                              return "Password not match";
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      // Register Button
+                      // ignore: sized_box_for_whitespace
+                      Container(
+                        width: double.infinity,
+                        child: TextButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                const Color.fromRGBO(177, 157, 143, 1.0)),
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            overlayColor:
+                                MaterialStateProperty.resolveWith<Color?>(
+                                    (Set<MaterialState> states) {
+                              if (states.contains(MaterialState.pressed)) {
+                                return const Color.fromRGBO(177, 157, 143, 1.0);
+                              }
+                              return null;
+                            }),
+                          ),
+                          onPressed: () async {
+                            if (_registerFormKey.currentState!.validate()) {
+                              final response = await request.post(
+                                  "http://10.0.2.2:8000/register",
+                                  jsonEncode(<String, String>{
+                                    'first_name':first_name,
+                                    'last_name':last_name,
+                                    'username': username,
+                                    'email': email,
+                                    'password1': password1,
+                                    'password2': password2,
+                                  }));
+
+                              if (response["status"] == true) {
+                                await request
+                                    .login("http://10.0.2.2:8000/login", {
+                                  'username': username,
+                                  'password': password1,
+                                });
+                                Navigator.of(context).pushNamedAndRemoveUntil(
+                                    '/', (Route<dynamic> route) => false);
+                              } else {
+                                _showErrors(context, response);
+                              }
+                            }
+                          },
+                          child: const Text(
+                            "Register",
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                RichText(
+                  text: TextSpan(
+                      text: "Already have an account?",
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 19, 19, 19),
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.popAndPushNamed(context, "/login");
+                        }),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
