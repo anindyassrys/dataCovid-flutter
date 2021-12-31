e33dc7e663baa5804f4aabd9c522ce27133974f3
Dixon Frederick <dixon.frederick@ui.ac.id>
2021-12-31 21:52:13 +0700

    kewaspadaan

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,140 @@
+//@dart = 2.9
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:http/http.dart' as http;
+import 'dart:io';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+final webViewKey = GlobalKey<WebViewContainerState>();
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Home extends StatefulWidget {
+  const Home({Key key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<Home> {
+  String _currentIndexes = 'Indeks Kewaspadaan';
+  String _currentSources = 'https://datawrapper.dwcdn.net/DfOth/5/';
+  List indexes = ['Indeks Kewaspadaan'];
+  List sources = ['https://datawrapper.dwcdn.net/DfOth/5/'];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  fetchData() async {
+    var url = Uri.parse('https://covid-information-app.herokuapp.com/kewaspadaan/jsonWaspada');
+    var response = await http.get(url);
+    var jsonData = jsonDecode(response.body);
+    indexes.clear();
+    indexes.add('Indeks Kewaspadaan');
+    sources.clear();
+    sources.add('https://datawrapper.dwcdn.net/DfOth/5/');
+
+    for (var item in jsonData) {
+      indexes.add(item['fields']['name'].toString());
+      sources.add(item['fields']['source'].toString());
+    }
+    return jsonData;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      appBar: AppBar(title: Text("Indeks Kewaspadaan")),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+                child: Column(
+                    children: <Widget>[
+                        Container(
+                            color: Colors.white,
+                            alignment: Alignment.center,
+                            height:100,
+                            child: Text('Indeks Kewaspadaan',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontFamily: 'Aleo',
+                                    fontStyle: FontStyle.normal,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 25.0,
+                                    ),
+                                ),
+                            ),
+                        FutureBuilder(
+                          future:fetchData(),
+                          builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                              return Container(
+                                  color: Colors.white,
+                                  alignment: Alignment(0.0, 0.5),
+                                  height:150,
+                                  width:350,
+                                  child: ListView(
+                                  children: <Widget>[
+                                      DropdownButtonFormField<String>(
+                                          value: _currentIndexes,
+                                          items: indexes.map((index) {
+                                            return DropdownMenuItem<String>(
+                                              value: index,
+                                              child: Text(index),
+                                            );
+                                          }).toList(),
+                                          onChanged: (val) => setState(() { _currentIndexes = val; _currentSources = sources.elementAt(indexes.indexOf(val)); webViewKey.currentState.reloadWebView(_currentSources);}), 
+                                          ),
+                                          Text(
+                                            "\nIndeks yang ditampilkan: \n$_currentIndexes",
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ],
+                                      ),
+                                  );
+                          } else {
+                              return Text("-");
+                          }
+                        }),
+                        WebViewContainer(key: webViewKey),
+                        ],
+                    ),
+                ),
     );
+  }
+}
+
+class WebViewContainer extends StatefulWidget {
+  WebViewContainer({Key key}) : super(key: key);
+
+  @override
+  WebViewContainerState createState() => WebViewContainerState();
+}
+
+class WebViewContainerState extends State<WebViewContainer> {
+  WebViewController _webViewController;
+  String _currentWebView = 'https://datawrapper.dwcdn.net/DfOth/5/';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        alignment: Alignment(0.0, -0.5),
+        height:350,
+        width:350,
+        child: WebView(
+            onWebViewCreated: (WebViewController controller) {
+              _webViewController = controller;
+            },
+            initialUrl: Uri.dataFromString('<html><body><iframe class = "test" title="Indeks Kewaspadaan" aria-label="Map" id="datawrapper-chart-DfOth" src="$_currentWebView" scrolling="no" frameborder="1" style="width: 0; min-width: 100% !important; border: none; padding: none;" height="392" ></iframe></body></html>', mimeType: 'text/html').toString(),
+            javascriptMode: JavascriptMode.unrestricted,
+        )
+      );
+  }
+
+  void reloadWebView(String replace) {
+    setState(() {_currentWebView = replace; _webViewController.loadUrl(_currentWebView);});
   }
 }
